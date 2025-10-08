d3a26ef0a3a4612b4f3468925a4f4ba133ba5506
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-10-08 17:19:22 +0200

    Dataformat java dsl

--- camel-upgrade-recipes/src/test/java/org/apache/camel/upgrade/CamelUpdate415Test.java
+++ camel-upgrade-recipes/src/test/java/org/apache/camel/upgrade/CamelUpdate415Test.java
@@ -32,7 +32,8 @@ public class CamelUpdate415Test implements RewriteTest {
     public void defaults(RecipeSpec spec) {
         CamelTestUtil.recipe(spec, CamelTestUtil.CamelVersion.v4_15)
           .parser(CamelTestUtil.parserFromClasspath(CamelTestUtil.CamelVersion.v4_14, "camel-langchain4j-chat",
-                  "camel-langchain4j-embeddings", "camel-milvus", "camel-neo4j", "camel-pinecone", "camel-qdrant", "camel-weaviate"))
+                  "camel-langchain4j-embeddings", "camel-milvus", "camel-neo4j", "camel-pinecone", "camel-qdrant", "camel-weaviate",
+                  "camel-core-model", "camel-spi", "camel-api", "camel-base-engine", "jakarta.xml.bind-api"))
           .typeValidationOptions(TypeValidation.none());
     }
 
@@ -87,149 +88,235 @@ public class CamelUpdate415Test implements RewriteTest {
                 """));
     }
 
-
     /**
      * <a href="https://camel.apache.org/manual/camel-4x-upgrade-guide-4_15.html#_data_formats">Data Formats</a>
      */
     @Test
-    void dataFormatsCryptoYamlTest() {
-        //language=yaml
-        rewriteRun(yaml(
+    void dataformatCryptoTest() {
+        //language=java
+        rewriteRun(java(
                 """
-                       route:
-                         id: route-2366
-                         from:
-                           id: start
-                           uri: direct
-                         parameters:
-                           name: start
-                           steps:
-                             - to:
-                               id: to-4212
-                               uri: crypto
-                               parameters:
-                                 algorithmParameterRef: 111
-                                 initVectorRef: 222
-                                 keyRef: 333
-                             - to:
-                               uri: csv
-                               parameters:
-                                 formatRef: 444
-                             - to:
-                               uri: csv
-                               parameters:
-                                 formatName: 555
-                             - to:
-                               uri: flatpack
-                               parameters:
-                                 parserFactoryRef: 666
-                             - to:
-                               uri: jaxb
-                               parameters:
-                                 namespacePrefixRef: 777
-                             - to:
-                               uri: soap
-                               parameters:
-                                 namespacePrefixRef: 888
-                                 elementNameStrategyRef: 999
-                             - to:
-                               uri: swiftMx
-                               parameters:
-                                 readConfigRef: 1111
-                                 writeConfigRef: 1222
-                             - to:
-                               uri: xmlSecurity
-                               parameters:
-                                 keyOrTrustStoreParametersRef: 1333
-                  """,
+                  import org.apache.camel.model.dataformat.CryptoDataFormat;
+                  import org.apache.camel.model.dataformat.CsvDataFormat;
+                  import org.apache.camel.model.dataformat.FlatpackCsvDataFormat;
+                  import org.apache.camel.model.dataformat.FlatpackDataFormat;
+                  import org.apache.camel.model.dataformat.JaxbDataFormat;
+                  import org.apache.camel.model.dataformat.SoapDataFormat;
+                  import org.apache.camel.model.dataformat.SwiftMxDataFormat;
+                  import org.apache.camel.model.dataformat.XmlSecurityDataFormat;
+      
+                  public class Cr {
+                    public void cryptoDataFormat() {
+                        CryptoDataFormat cfd = new CryptoDataFormat();
+                        cfd.setAlgorithmParameterRef("111");
+                        cfd.setKeyRef("222");
+                        cfd.setInitVectorRef("333");
+
+                        CsvDataFormat csv = new CsvDataFormat();
+                        csv.setFormatRef("444");
+                        csv.setFormatName("555");
+
+                        new FlatpackDataFormat().setParserFactoryRef("666");
+
+                        new JaxbDataFormat().setNamespacePrefixRef("777");
+
+                        SoapDataFormat soap = new SoapDataFormat();;
+
+                        soap.setNamespacePrefixRef("888");
+                        soap.setElementNameStrategyRef("999");
+
+                        SwiftMxDataFormat swift = new SwiftMxDataFormat();
+                        swift.setReadConfigRef("1111");
+                        swift.setWriteConfigRef("1222");
+                        
+//                        new XMLSecurityDataFormat().setKeyOrTrustStoreParametersRef("1333");
+                    }
+                  }
+                  """, //there is missing a reference for proper parsing of XMLSecurityDataFormat
                 """
-                       route:
-                         id: route-2366
-                         from:
-                           id: start
-                           uri: direct
-                         parameters:
-                           name: start
-                           steps:
-                             - to:
-                               id: to-4212
-                               uri: crypto
-                               parameters:
-                                 algorithmParameterSpec: 111
-                                 initVector: 222
-                                 key: 333
-                             - to:
-                               uri: csv
-                               parameters:
-                                 format: 444
-                             - to:
-                               uri: csv
-                               parameters:
-                                 format: 555
-                             - to:
-                               uri: flatpack
-                               parameters:
-                                 parserFactory: 666
-                             - to:
-                               uri: jaxb
-                               parameters:
-                                 namespacePrefix: 777
-                             - to:
-                               uri: soap
-                               parameters:
-                                 namespacePrefix: 888
-                                 elementNameStrategy: 999
-                             - to:
-                               uri: swiftMx
-                               parameters:
-                                 readConfig: 1111
-                                 writeConfig: 1222
-                             - to:
-                               uri: xmlSecurity
-                               parameters:
-                                 keyOrTrustStoreParameters: 1333
-                  """));
+
+                  import org.apache.camel.model.dataformat.CryptoDataFormat;
+                  import org.apache.camel.model.dataformat.CsvDataFormat;
+                  import org.apache.camel.model.dataformat.FlatpackCsvDataFormat;
+                  import org.apache.camel.model.dataformat.FlatpackDataFormat;
+                  import org.apache.camel.model.dataformat.JaxbDataFormat;
+                  import org.apache.camel.model.dataformat.SoapDataFormat;
+                  import org.apache.camel.model.dataformat.SwiftMxDataFormat;
+                  import org.apache.camel.model.dataformat.XmlSecurityDataFormat;
+      
+                  public class Cr {
+                    public void cryptoDataFormat() {
+                        CryptoDataFormat cfd = new CryptoDataFormat();
+                        cfd.setAlgorithmParameterSpec("111");
+                        cfd.setKey("222");
+                        cfd.setInitVector("333");
+                        
+                        CsvDataFormat csv = new CsvDataFormat();
+                        csv.setFormat("444");
+                        csv.setFormat("555");
+                        
+                        new FlatpackDataFormat().setParserFactory("666");
+                        
+                        new JaxbDataFormat().setNamespacePrefix("777");
+                        
+                        SoapDataFormat soap = new SoapDataFormat();;
+                      
+                        soap.setNamespacePrefix("888");
+                        soap.setElementNameStrategy("999");
+                        
+                        SwiftMxDataFormat swift = new SwiftMxDataFormat();
+                        swift.setReadConfig("1111");
+                        swift.setWriteConfig("1222");
+                        
+//                        new XMLSecurityDataFormat().setKeyOrTrustStoreParametersRef("1333");
+                    }
+                  }
+                """));
     }
-
-
-    /**
-     *
-     */
-    @Test
-    void dataFormatsCryptoXmlTest() {
-        //language=xml
-        rewriteRun(xml(
-                """
-                  <camelContext>
-                    <route>
-                      <from uri="direct:start"/>
-                      <to uri="crypto:something" algorithmParameterRef='111' initVectorRef='222' keyRef='333'/>
-                      <to uri="csv" formatRef='444'/>
-                      <to uri="csv" formatName='555'/>
-                      <to uri="flatpack" parserFactoryRef='666'/>
-                      <to uri="jaxb" namespacePrefixRef='777'/>
-                      <to uri="soap" namespacePrefixRef='888' elementNameStrategyRef='999'/>
-                      <to uri="swiftMx" readConfigRef='1111' writeConfigRef='1222'/>
-                      <to uri="xmlSecurity" keyOrTrustStoreParametersRef='1333'/>
-                    </route>
-                  </camelContext>
-                  """,
-                """
-                  <camelContext>
-                    <route>
-                      <from uri="direct:start"/>
-                      <to uri="crypto:something" algorithmParameterSpec='111' key='333' initVector='222'/>
-                      <to uri="csv" format='444'/>
-                      <to uri="csv" format='555'/>
-                      <to uri="flatpack" parserFactory='666'/>
-                      <to uri="jaxb" namespacePrefix='777'/>
-                      <to uri="soap" namespacePrefix='888' elementNameStrategy='999'/>
-                      <to uri="swiftMx" readConfig='1111' writeConfig='1222'/>
-                      <to uri="xmlSecurity" keyOrTrustStoreParameters='1333'/>
-                    </route>
-                  </camelContext>
-                  """));
-
-    }
+//
+//
+//    /**
+//     * <a href="https://camel.apache.org/manual/camel-4x-upgrade-guide-4_15.html#_data_formats">Data Formats</a>
+//     */
+//    @Test
+//    void dataFormatsCryptoYamlTest() {
+//        //language=yaml
+//        rewriteRun(yaml(
+//                """
+//                       route:
+//                         id: route-2366
+//                         from:
+//                           id: start
+//                           uri: direct
+//                         parameters:
+//                           name: start
+//                           steps:
+//                             - to:
+//                               id: to-4212
+//                               uri: crypto
+//                               parameters:
+//                                 algorithmParameterRef: 111
+//                                 initVectorRef: 222
+//                                 keyRef: 333
+//                             - to:
+//                               uri: csv
+//                               parameters:
+//                                 formatRef: 444
+//                             - to:
+//                               uri: csv
+//                               parameters:
+//                                 formatName: 555
+//                             - to:
+//                               uri: flatpack
+//                               parameters:
+//                                 parserFactoryRef: 666
+//                             - to:
+//                               uri: jaxb
+//                               parameters:
+//                                 namespacePrefixRef: 777
+//                             - to:
+//                               uri: soap
+//                               parameters:
+//                                 namespacePrefixRef: 888
+//                                 elementNameStrategyRef: 999
+//                             - to:
+//                               uri: swiftMx
+//                               parameters:
+//                                 readConfigRef: 1111
+//                                 writeConfigRef: 1222
+//                             - to:
+//                               uri: xmlSecurity
+//                               parameters:
+//                                 keyOrTrustStoreParametersRef: 1333
+//                  """,
+//                """
+//                       route:
+//                         id: route-2366
+//                         from:
+//                           id: start
+//                           uri: direct
+//                         parameters:
+//                           name: start
+//                           steps:
+//                             - to:
+//                               id: to-4212
+//                               uri: crypto
+//                               parameters:
+//                                 algorithmParameterSpec: 111
+//                                 initVector: 222
+//                                 key: 333
+//                             - to:
+//                               uri: csv
+//                               parameters:
+//                                 format: 444
+//                             - to:
+//                               uri: csv
+//                               parameters:
+//                                 format: 555
+//                             - to:
+//                               uri: flatpack
+//                               parameters:
+//                                 parserFactory: 666
+//                             - to:
+//                               uri: jaxb
+//                               parameters:
+//                                 namespacePrefix: 777
+//                             - to:
+//                               uri: soap
+//                               parameters:
+//                                 namespacePrefix: 888
+//                                 elementNameStrategy: 999
+//                             - to:
+//                               uri: swiftMx
+//                               parameters:
+//                                 readConfig: 1111
+//                                 writeConfig: 1222
+//                             - to:
+//                               uri: xmlSecurity
+//                               parameters:
+//                                 keyOrTrustStoreParameters: 1333
+//                  """));
+//    }
+//
+//
+//    /**
+//     *
+//     */
+//    @Test
+//    void dataFormatsCryptoXmlTest() {
+//        //language=xml
+//        rewriteRun(xml(
+//                """
+//                  <camelContext>
+//                    <route>
+//                      <from uri="direct:start"/>
+//                      <to uri="crypto:something" algorithmParameterRef='111' initVectorRef='222' keyRef='333'/>
+//                      <to uri="csv" formatRef='444'/>
+//                      <to uri="csv" formatName='555'/>
+//                      <to uri="flatpack" parserFactoryRef='666'/>
+//                      <to uri="jaxb" namespacePrefixRef='777'/>
+//                      <to uri="soap" namespacePrefixRef='888' elementNameStrategyRef='999'/>
+//                      <to uri="swiftMx" readConfigRef='1111' writeConfigRef='1222'/>
+//                      <to uri="xmlSecurity" keyOrTrustStoreParametersRef='1333'/>
+//                    </route>
+//                  </camelContext>
+//                  """,
+//                """
+//                  <camelContext>
+//                    <route>
+//                      <from uri="direct:start"/>
+//                      <to uri="crypto:something" algorithmParameterSpec='111' key='333' initVector='222'/>
+//                      <to uri="csv" format='444'/>
+//                      <to uri="csv" format='555'/>
+//                      <to uri="flatpack" parserFactory='666'/>
+//                      <to uri="jaxb" namespacePrefix='777'/>
+//                      <to uri="soap" namespacePrefix='888' elementNameStrategy='999'/>
+//                      <to uri="swiftMx" readConfig='1111' writeConfig='1222'/>
+//                      <to uri="xmlSecurity" keyOrTrustStoreParameters='1333'/>
+//                    </route>
+//                  </camelContext>
+//                  """));
+//
+//    }
 
 }
