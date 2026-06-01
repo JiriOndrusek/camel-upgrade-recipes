f8959be4a2c913200d6864bc5a4c8beee9425968
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2026-06-01 15:12:17 +0200

    more header tests

--- camel-upgrade-recipes/src/test/java/org/apache/camel/upgrade/CamelUpdate421Test.java
+++ camel-upgrade-recipes/src/test/java/org/apache/camel/upgrade/CamelUpdate421Test.java
@@ -74,7 +74,7 @@ public class CamelUpdate421Test implements RewriteTest {
                         - route:
                             from:
                               uri: "direct:start"
-                            steps:
+                              steps:
                               - setHeader:
                                   name: kafka.TOPIC
                                   constant: my-topic
@@ -83,7 +83,7 @@ public class CamelUpdate421Test implements RewriteTest {
                         - route:
                             from:
                               uri: "direct:start"
-                            steps:
+                              steps:
                               - setHeader:
                                   name: CamelKafkaTopic
                                   constant: my-topic
@@ -103,7 +103,7 @@ public class CamelUpdate421Test implements RewriteTest {
                         """
                         import org.apache.camel.Exchange;
                         import org.apache.camel.builder.RouteBuilder;
-        
+
                         class Test extends RouteBuilder {
                             public void configure() {
                                 from("direct:start")
@@ -117,7 +117,7 @@ public class CamelUpdate421Test implements RewriteTest {
                         """
                         import org.apache.camel.Exchange;
                         import org.apache.camel.builder.RouteBuilder;
-        
+
                         class Test extends RouteBuilder {
                             public void configure() {
                                 from("direct:start")
@@ -125,6 +125,328 @@ public class CamelUpdate421Test implements RewriteTest {
                                         exchange.getIn().setHeader("CamelKafkaTopic", "topic1");
                                     })
                                     .setBody(simple("${header.CamelKafkaTopic}"));
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testDnsHeadersMigrationJava() {
+        // Test camel-dns header migration in Java - Message API and Simple expressions
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("dns.name", "www.example.com");
+                                        exchange.getIn().setHeader("dns.server", "8.8.8.8");
+                                    })
+                                    .setBody(simple("${header.dns.name}"))
+                                    .to("dns:lookup");
+                            }
+                        }
+                        """,
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CamelDnsName", "www.example.com");
+                                        exchange.getIn().setHeader("CamelDnsServer", "8.8.8.8");
+                                    })
+                                    .setBody(simple("${header.CamelDnsName}"))
+                                    .to("dns:lookup");
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testDnsHeadersMigrationXml() {
+        // Test camel-dns header migration in XML DSL - setHeader name attributes
+        //language=xml
+        rewriteRun(
+                xml(
+                        """
+                        <route xmlns="http://camel.apache.org/schema/spring">
+                            <from uri="direct:start"/>
+                            <setHeader name="dns.name">
+                                <constant>www.example.com</constant>
+                            </setHeader>
+                            <setHeader name="dns.type">
+                                <constant>A</constant>
+                            </setHeader>
+                            <setHeader name="term">
+                                <constant>search-term</constant>
+                            </setHeader>
+                            <to uri="dns:lookup"/>
+                        </route>
+                        """,
+                        """
+                        <route xmlns="http://camel.apache.org/schema/spring">
+                            <from uri="direct:start"/>
+                            <setHeader name="CamelDnsName">
+                                <constant>www.example.com</constant>
+                            </setHeader>
+                            <setHeader name="CamelDnsType">
+                                <constant>A</constant>
+                            </setHeader>
+                            <setHeader name="CamelDnsTerm">
+                                <constant>search-term</constant>
+                            </setHeader>
+                            <to uri="dns:lookup"/>
+                        </route>
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testDnsHeadersMigrationYaml() {
+        // Test camel-dns header migration in YAML DSL - setHeader name
+        //language=yaml
+        rewriteRun(
+                yaml(
+                        """
+                        - route:
+                            from:
+                              uri: "direct:start"
+                              steps:
+                              - setHeader:
+                                  name: dns.name
+                                  constant: www.example.com
+                              - setHeader:
+                                  name: dns.server
+                                  constant: 8.8.8.8
+                              - setHeader:
+                                  name: term
+                                  constant: search-term
+                              - to:
+                                  uri: "dns:lookup"
+                        """,
+                        """
+                        - route:
+                            from:
+                              uri: "direct:start"
+                              steps:
+                              - setHeader:
+                                  name: CamelDnsName
+                                  constant: www.example.com
+                              - setHeader:
+                                  name: CamelDnsServer
+                                  constant: 8.8.8.8
+                              - setHeader:
+                                  name: CamelDnsTerm
+                                  constant: search-term
+                              - to:
+                                  uri: "dns:lookup"
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testLuceneHeadersMigration() {
+        // Test camel-lucene header migration
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("QUERY", "test query");
+                                        exchange.getIn().setHeader("RETURN_LUCENE_DOCS", true);
+                                    })
+                                    .to("lucene:search");
+                            }
+                        }
+                        """,
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CamelLuceneQuery", "test query");
+                                        exchange.getIn().setHeader("CamelLuceneReturnLuceneDocs", true);
+                                    })
+                                    .to("lucene:search");
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testCouchdbHeadersMigration() {
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CouchDbDatabase", "mydb");
+                                        exchange.getIn().setHeader("CouchDbId", "doc1");
+                                    })
+                                    .to("couchdb:http://localhost:5984");
+                            }
+                        }
+                        """,
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CamelCouchDbDatabase", "mydb");
+                                        exchange.getIn().setHeader("CamelCouchDbId", "doc1");
+                                    })
+                                    .to("couchdb:http://localhost:5984");
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testCouchbaseHeadersMigration() {
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CCB_KEY", "key1");
+                                        exchange.getIn().setHeader("CCB_TTL", 3600);
+                                    })
+                                    .to("couchbase:http://localhost");
+                            }
+                        }
+                        """,
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CamelCouchbaseKey", "key1");
+                                        exchange.getIn().setHeader("CamelCouchbaseTtl", 3600);
+                                    })
+                                    .to("couchbase:http://localhost");
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testJGroupsRaftHeadersMigration() {
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("JGROUPSRAFT_SET_OFFSET", 10);
+                                        exchange.getIn().setHeader("JGROUPSRAFT_SET_TIMEOUT", 5000);
+                                    })
+                                    .to("jgroups-raft:cluster");
+                            }
+                        }
+                        """,
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CamelJGroupsRaftSetOffset", 10);
+                                        exchange.getIn().setHeader("CamelJGroupsRaftSetTimeout", 5000);
+                                    })
+                                    .to("jgroups-raft:cluster");
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void testShiroHeadersMigration() {
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("SHIRO_SECURITY_USERNAME", "admin");
+                                        exchange.getIn().setHeader("SHIRO_SECURITY_PASSWORD", "secret");
+                                    })
+                                    .to("shiro:login");
+                            }
+                        }
+                        """,
+                        """
+                        import org.apache.camel.Exchange;
+                        import org.apache.camel.builder.RouteBuilder;
+
+                        class Test extends RouteBuilder {
+                            public void configure() {
+                                from("direct:start")
+                                    .process(exchange -> {
+                                        exchange.getIn().setHeader("CamelShiroSecurityUsername", "admin");
+                                        exchange.getIn().setHeader("CamelShiroSecurityPassword", "secret");
+                                    })
+                                    .to("shiro:login");
                             }
                         }
                         """
